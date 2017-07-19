8914f443ce4e68e8a0895f1bb48c1755b0e803e8
SirRawlins <robrawlins@gmail.com>
2017-07-19 10:15:26 +0100

    Fixed a whole bunch of typos and spelling mistakes, mainly in the HTML comments, no big deal, just a general tidyup.

--- src/javascripts/smart-anchor.js
+++ src/javascripts/smart-anchor.js
@@ -1,13 +1,13 @@
 /*
  *
- * This is a small plugin which allows you to create acnhor links which
+ * This is a small plugin which allows you to create anchor links which
  * only link when the target isn't already visible within the viewport.
  *
  * We use this within the theme for the support link under the all systems are
  * go, as on some screen sizes the support links are already visible, but they
  * aren't on others of if large amounts of content are on the page.
  *
- * Depenacies: is-element-in-viewport.js
+ * Dependencies: is-element-in-viewport.js
  *
  * Usage: <a data-smart-anchor="#target"> instead of <a href="#target">
  *
@@ -15,13 +15,13 @@
 // Invoke on load, scroll and window change.
 $(window).on('DOMContentLoaded load resize scroll', function(e) {
 
-	// Get all the smart anchor elents on the page.
+	// Get all the smart anchor elements on the page.
 	$("a[data-smart-anchor]").each(function() {
 		// Get the target of the data attribute.
 		var $target = $($(this).data('smart-anchor'));
 
 		// Toggle the href attribute on the link based on the visibility
-		// of the targer element so if it's visible we don't link to it.
+		// of the target element so if it's visible we don't link to it.
 		if (isElementInViewport($target)) {
 			// The element is currently visible, make sure the href attribute
 			// is removed from the link.
